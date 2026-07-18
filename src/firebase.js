232004843f713ca16590ce8497784b42c6679b0e
Cjansen1988 <cjansen@mail.de>
2026-07-18 13:38:02 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCDoNvmvv8tgUQ9CM1tYCQ7JsSEF1QF6P0",
   authDomain: "tusharenspielvorbereitung.firebaseapp.com",
@@ -13,9 +9,10 @@ const firebaseConfig = {
   storageBucket: "tusharenspielvorbereitung.firebasestorage.app",
   messagingSenderId: "640969364886",
   appId: "1:640969364886:web:7155800f829bda442939b7",
-  measurementId: "G-0HSY59JJHG"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const COLLECTION = "tusharen2";
